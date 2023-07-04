3a146918fda4197bc8398d6d2d03d448cd0aecde
Minju Kang <97059373+Minju9187@users.noreply.github.com>
2023-07-04 23:06:17 +0900

    Add files via upload

---
+++ 코딩테스트연습/ihatesamenum.js
@@ -0,0 +1,14 @@
+function solution(arr)
+{
+    var answer = [];
+    for(let i = 0; i < arr.length - 1; i++){
+        if(arr[i] != arr[i+1]){
+            answer.push(arr[i]);
+        }
+    }
+    answer.push(arr[arr.length - 1]);
+    return answer;
+}
+
+//처음에는 뒤에꺼랑 비교해서 같으면 앞에것을 push하는 것으로 생각해봤는데 그렇게 되면 3개 이상 연속으로 나오게 됐을 때 안됨
+//그래서 비교해서 달라질 때 앞에것을 push하도록 함.
