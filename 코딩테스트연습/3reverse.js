7b87f165b706dcc869c6af22687900aeedfca822
Minju Kang <97059373+Minju9187@users.noreply.github.com>
2023-07-08 13:55:45 +0900

    3reverse.js

--- 코딩테스트연습/3reverse.js
+++ 코딩테스트연습/3reverse.js
@@ -1,4 +1,4 @@
-// /https://school.programmers.co.kr/learn/courses/30/lessons/12930
+//https://school.programmers.co.kr/learn/courses/30/lessons/68935
 function solution(n) {
     var answer = 0;
     var str3,str3_reverse;
