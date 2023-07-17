16833585d1eadb7ed6521e0bce133094d506b309
Minju Kang <97059373+Minju9187@users.noreply.github.com>
2023-07-17 18:22:55 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -0,0 +1,15 @@
+function solution(n) {
+    var answer = 0;
+    var str3,str3_reverse;
+
+    str3 = n.toString(3);
+    //reverse 사용, 
+    str3_reverse = str3.split("").reverse().join("");
+
+    answer = Number.parseInt(str3_reverse, 3);
+    return answer;
+}
+
+//진법 변환 10진법 -> 다른진법 & 다른진법 -> 10진법 변환 방법 https://medium.com/web-dev-note/javascript-%EC%A7%84%EB%B2%95-%EB%B3%80%ED%99%98-330694083495
+
+//reverse사용법 https://www.freecodecamp.org/korean/news/how-to-reverse-a-string-in-javascript-in-3-different-ways/
