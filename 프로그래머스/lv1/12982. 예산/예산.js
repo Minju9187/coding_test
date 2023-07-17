8a1bd603eb5f8659eaaa7fec8d112daaa5756ed0
Minju Kang <97059373+Minju9187@users.noreply.github.com>
2023-07-17 18:23:13 +0900

    [level 1] Title: 예산, Time: 0.13 ms, Memory: 33 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12982. 예산/예산.js
@@ -0,0 +1,14 @@
+function solution(d, budget) {
+    var answer = 0;
+    var sum = 0;
+    d.sort((a, b) => a - b)
+    for( let i = 0 ;i< d.length ; i++){
+        sum += d[i];
+        if(sum > budget){
+            break;
+        }
+        answer += 1;
+    }
+    
+    return answer;
+}
