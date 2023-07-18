648083632cb0e0191e27c2d9e798a032336a8db6
Minju Kang <97059373+Minju9187@users.noreply.github.com>
2023-07-18 18:03:56 +0900

    [unrated] Title: 코드 처리하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.js
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.js
@@ -1,14 +1,18 @@
 function solution(code) {
-    let answer = '';
+    let ret = '';
     let mode = 0;
 
     for (let i = 0; i < code.length; i += 1) {
       if (Number(code[i]) === 1) {
-        mode = mode === 1 ? 0 : 1;
+          if(mode == 1)
+              mode = 0
+          else mode = 1
       }
       if (Number(code[i]) !== 1 && i % 2 === mode) {
-        answer += code[i];
+        ret += code[i];
       }
     }
-    return answer.length > 0 ? answer : 'EMPTY';
+    if(ret.length > 0)
+        return ret;
+    else return "EMPTY"
 }
