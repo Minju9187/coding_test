f7624b13bb72ad7d78d3c13d4e7f42c0375c6232
Minju Kang <97059373+Minju9187@users.noreply.github.com>
2023-07-19 01:12:07 +0900

    [unrated] Title: 크기가 작은 부분문자열, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
+++ 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -1,13 +1,15 @@
+// substring이나 slice 둘다 써도 됨
+// parseInt랑 Number 둘다 써도 가능
 function solution(t, p) {
     var answer = 0;
     let n = p.length;
     let str = "";
     for(let i=0;i<t.length - n+1;i++){
-        // t.slice(idx,idx+pLength)
-        str = t.slice(i,i+n);
+        str = t.substring(i,i+n);
         if(p>=parseInt(str)){
             answer+=1;
         }
     }
     return answer;
+    //
 }
