cae794152cb36104504435a442720bdfcbc26e67
Minju Kang <97059373+Minju9187@users.noreply.github.com>
2023-07-27 09:32:31 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,15 +1,19 @@
 function solution(s) {
     var answer = 0;
-    s=replaceAll(s,'zero',0);
-    s=replaceAll(s,'one',1);
-    s=replaceAll(s,'two',2);
-    s=replaceAll(s,'three',3);
-    s=replaceAll(s,'four',4);
-    s=replaceAll(s,'five',5);
-    s=replaceAll(s,'six',6);
-    s=replaceAll(s,'seven',7);
-    s=replaceAll(s,'eight',8);
-    s=replaceAll(s,'nine',9);
+    arr = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    for(var i=0;i<arr.length;i++){
+        s = replaceAll(s, arr[i], i);
+    }
+    // s=replaceAll(s,'zero',0);
+    // s=replaceAll(s,'one',1);
+    // s=replaceAll(s,'two',2);
+    // s=replaceAll(s,'three',3);
+    // s=replaceAll(s,'four',4);
+    // s=replaceAll(s,'five',5);
+    // s=replaceAll(s,'six',6);
+    // s=replaceAll(s,'seven',7);
+    // s=replaceAll(s,'eight',8);
+    // s=replaceAll(s,'nine',9);
     return Number(s);
 }
 // serchStr을 기준으로 쪼갠 후 그 곳에 대체 텍스트를 넣고 합쳐준다.
