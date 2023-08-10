f2c7b77300b8147bc8e9c95c5954f546a0a27ac4
Minju Kang <97059373+Minju9187@users.noreply.github.com>
2023-08-11 03:15:19 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,12 +1,9 @@
 function solution(s) {
     var answer = '';
-    let min,max;
-    let arr,numArr;
+    let arr;
     arr = s.split(" ");
     answer+=Math.min(...arr);
     answer+=(" ");
     answer+=Math.max(...arr);
     return answer;
 }
-
-//Javascript에서 배열의 문자를 숫자로 변환하는 법 https://programming4myself.tistory.com/41
