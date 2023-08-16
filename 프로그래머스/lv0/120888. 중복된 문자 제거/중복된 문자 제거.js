fea779059d67e599465e28423163ac56ffe5502c
Minju Kang <97059373+Minju9187@users.noreply.github.com>
2023-08-17 03:36:08 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.js
+++ 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.js
@@ -1,6 +1,7 @@
 function solution(my_string) {
     var answer = '';
     let idx = 0;
+    // const answer = my_string => my_string.filter((v,i,self) => self.indexOf(v) === i);
     for(let i=0;i<my_string.length;i++){
         idx = 0;
         for(let j=i;j>=0;j--){
