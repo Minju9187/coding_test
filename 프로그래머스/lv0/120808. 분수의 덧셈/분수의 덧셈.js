1ea0913011443dd897a9a065fcddb2fcc5b361d6
Minju Kang <97059373+Minju9187@users.noreply.github.com>
2023-08-18 03:22:01 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.js
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -1,31 +1,11 @@
 function solution(numer1, denom1, numer2, denom2) {
-    var answer = [];
-    let a,b;
-    let denom;
-    let k=1;
-    denom = getLCM(denom1,denom2);
-    a = numer1*(denom/denom1);
-    b = numer2*(denom/denom2);
-    
-    for(let i=0;i<denom+1;i++){
-        if((a+b)%i==0&&(denom)%i==0){
-           k = i; 
+    let Top = numer1 * denom2 + numer2 * denom1
+    let Bot = denom1 * denom2
+    let min = 1
+    for (let i=1; i<=Bot; i++){
+        if (Top%i === 0 && Bot%i === 0) {
+            min = i
         }
     }
-    
-    answer[0] = (a + b)/k;
-    answer[1] = denom/k;
-    return answer;
-}
-
-function getLCM(num1, num2){
-	let lcm = 1;
-   
-    while(true){
-      if((lcm % num1 == 0) && (lcm % num2 == 0)){
-        break;
-      }
-      lcm++;
-    }
-  	return lcm
+    return [Top/min, Bot/min]
 }
