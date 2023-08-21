854fd3caf02408e24cef0ae5a031e3906ac850e2
Minju Kang <97059373+Minju9187@users.noreply.github.com>
2023-08-21 22:18:02 +0900

    [unrated] Title: 정수를 나선형으로 배치하기, Time: 0.35 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.js
+++ 프로그래머스/unrated/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.js
@@ -6,20 +6,21 @@ function solution(n) {
     var answer = Array.from(Array(n), ()=>new Array(n).fill(0));
     for(let i =1;i<=Math.pow(n,2);i++){
         answer[a][b] = i;
-        console.log(a,b)
         a+=dir[key][0];
         b+=dir[key][1];
+        // 배열 바깥으로 나갔을 때 전 행열로 돌려놓고 key값을 바꾼다. 
         if(b==n||a==n||b==-1||a==-1){
             a-=dir[key][0];
             b-=dir[key][1];
-            key = (key + 1)%4;
+            key = (key + 1) % 4;
             a+=dir[key][0];
             b+=dir[key][1];
         }
+        // 배열 값이 차있는 경우 전 행열로 돌려놓고 key값을 바꾼다.
         else if(answer[a][b]!=0){
             a-=dir[key][0];
             b-=dir[key][1];
-            key = (key + 1)%4;
+            key = (key + 1) % 4;
             a+=dir[key][0];
             b+=dir[key][1];
         }
