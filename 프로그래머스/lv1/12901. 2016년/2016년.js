d005287535136e8243e0d67b039c9cf0e444063a
Minju Kang <97059373+Minju9187@users.noreply.github.com>
2023-08-31 01:15:41 +0900

    [level 1] Title: 2016년, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12901. 2016년/2016년.js
@@ -0,0 +1,13 @@
+function solution(a, b) {
+    var answer = '';
+    let week = ['FRI','SAT','SUN','MON','TUE','WED','THU'];
+    let month = [31,29,31,30,31,30,31,31,30,31,30,31]
+    let sum = 0;
+    for(let i = 0;i < a-1;i++){
+        sum += month[i];
+    }
+    sum += b;
+    sum -= 1;
+    answer = week[sum%7];
+    return answer;
+}
