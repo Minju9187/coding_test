3621f2260e2cb2a8d7fe7439031d72bde0a7fb25
Minju Kang <97059373+Minju9187@users.noreply.github.com>
2023-08-31 16:47:42 +0900

    [unrated] Title: 카드 뭉치, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.js
@@ -1,12 +1,22 @@
 function solution(cards1, cards2, goal) {
-    while(goal.length > 0){
-        if(goal[0] === cards1[0]){
-            goal.shift()
-            cards1.shift()
-        }else if(goal[0] === cards2[0]){
-            goal.shift()
-            cards2.shift()
-        }else return "No"
+    for(let i=0;i<goal.length;i++){
+        if(goal[i]==cards1[0]){
+            cards1.shift();
+        }
+        else if(goal[i]==cards2[0]){
+            cards2.shift();
+        }
+        else return 'No';
     }
-    return "Yes";
+    return 'Yes';
+    // while(goal.length > 0){
+    //     if(goal[0] === cards1[0]){
+    //         goal.shift()
+    //         cards1.shift()
+    //     }else if(goal[0] === cards2[0]){
+    //         goal.shift()
+    //         cards2.shift()
+    //     }else return "No"
+    // }
+    // return "Yes";
 }
