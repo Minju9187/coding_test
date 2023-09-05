894871747daee420b4c770b93c972a756b38cc26
Minju Kang <97059373+Minju9187@users.noreply.github.com>
2023-09-05 17:03:49 +0900

    [level 2] Title: 올바른 괄호, Time: 2.50 ms, Memory: 37.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,14 +1,28 @@
 function solution(s){
-    var answer = true;
-    let stack = [];
-    for(let i=0;i<s.length;i++){
-        if(s[i]==='('){
-            stack.push('(');
-        }
-        else{
-            if(stack[stack.length-1]==='(')stack.pop();
-            else return false;
+    let cnt = 0;
+
+    for(let i = 0; i<s.length; i++){
+        s[i] === "(" ? cnt +=1 : cnt+= -1
+        if(cnt<0){
+            break;
         }
     }
-    return !stack.length ? true : false;
+
+    return cnt === 0 ? true : false;
 }
+// function solution(s){
+//     var answer = true;
+//     let stack = [];
+//     for(let i=0;i<s.length;i++){
+//         if(s[i]==='('){
+//             stack.push('(');
+//         }
+//         else{
+//             if(stack[stack.length-1]==='(')stack.pop();
+//             else return false;
+//         }
+//     }
+//     //if(stack.length>0)return false;
+//     //else return true;
+//     return !stack.length ? true : false;
+// }
