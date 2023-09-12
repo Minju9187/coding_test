d0f70533782b1e49df8d5ecad8b35f6727c15447
Minju Kang <97059373+Minju9187@users.noreply.github.com>
2023-09-12 18:55:27 +0900

    [unrated] Title: 덧칠하기, Time: 12.44 ms, Memory: 39.2 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.js
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.js
@@ -1,13 +1,32 @@
 function solution(n, m, section) {
-    var answer = 0;
-    for(let i=0;i<section.length;i++){
-        for(let j=i+1;j<section.length;j++){
-            if(section[j]-section[i]>=m){
-                answer++;
-                i=j;
-            }
+    let answer = 0;
+    //벽의 길이 n
+    //롤러의 길이 m
+    //section 칠해야하는 벽 구간
+    const wall = Array(n).fill(1);
+    section.forEach(v=>{
+        wall[v-1] = 0;
+    })
+    console.log(wall)
+    wall.forEach((v)=>{
+        if(v === 0){
+            wall.splice(0, m, 1);
+            answer++;
         }
-    }
-    answer++;
+    })
+    
     return answer;
 }
+// function solution(n, m, section) {
+//     var answer = 0;
+//     for(let i=0;i<section.length;i++){
+//         for(let j=i+1;j<section.length;j++){
+//             if(section[j]-section[i]>=m){
+//                 answer++;
+//                 i=j;
+//             }
+//         }
+//     }
+//     answer++;
+//     return answer;
+// }
