84d6a70d0f1f8d7d8339cd676306aa064b757922
Minju Kang <97059373+Minju9187@users.noreply.github.com>
2023-09-26 14:19:02 +0900

    [unrated] Title: 옹알이 (2), Time: 0.54 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.js
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.js
@@ -2,11 +2,16 @@ function solution(babbling) {
     var answer = 0;
     let arr;
     let key;
-    arr = babbling.map( v => v.replace(/aya/g,'A').replace(/ye/g,'B').replace(/woo/g,'C').replace(/ma/g,'D')).filter(word=>!word.match(/[a-z]/g))
+    arr = babbling.map( v => v.replace(/aya/g,'A')
+                       .replace(/ye/g,'B')
+                       .replace(/woo/g,'C')
+                       .replace(/ma/g,'D'))
+                        .filter(word=>!word.match(/[a-z]/g))
+    
     for(let i=0;i<arr.length; i++){
         key = 0;
         for(let j=0;j<arr[i].length-1;j++){
-            if(arr[i][j]==arr[i][j+1]){
+            if(arr[i][j]===arr[i][j+1]){
                 key = 1;
                 break;
             }
