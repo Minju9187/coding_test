4beb76afa17f565b95e11d45125f2316f0d38ef3
Minju Kang <97059373+Minju9187@users.noreply.github.com>
2023-10-13 16:13:08 +0900

    [level 1] Title: 둘만의 암호, Time: 0.29 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.js
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.js
@@ -2,12 +2,10 @@ function solution(s, skip, index) {
     var answer = '';
     let arrSkipNum = [];
     let arrSNum = [];
-    let cnt=0;
     
-    let arrSkip = skip.split("").sort().map(v=>v.charCodeAt());
+    let arrSkip = skip.split("").map(v=>v.charCodeAt());
     arrSNum = s.split("").map(v=>v.charCodeAt())
     for(let i=0;i<arrSNum.length;i++){
-        cnt=0;
         for(let j=1;j<=index;j++){
             arrSNum[i]++;
             if(arrSkip.includes((arrSNum[i]-97)%26+97))j--;
