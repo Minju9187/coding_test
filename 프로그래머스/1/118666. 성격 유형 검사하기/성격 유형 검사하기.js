1313826775c154f7ab53c94366eb6e475b7b5e38
Minju Kang <97059373+Minju9187@users.noreply.github.com>
2023-12-06 14:44:42 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.66 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -1,28 +1,22 @@
 function solution(survey, choices) {
-  const types = { R: 0, T: 0, C: 0, F: 0, J: 0, M: 0, A: 0, N: 0 };
-  let answer = "";
-  for (let i = 0; i < choices.length; i++) {
-    const [leftType, rightType] = survey[i].split("");
-    const result = Math.abs(choices[i] - 4);
-    if (choices[i] < 4) {
-      // 비동의 -> 왼쪽 요소
-      types[leftType] += result;
-    } else if (choices[i] > 4) {
-      // 동의 -> 오른쪽 원소
-      types[rightType] += result;
+    const types = { R: 0, T: 0, C: 0, F: 0, J: 0, M: 0, A: 0, N: 0 };
+    let answer = "";
+    for(let i=0;i<survey.length;i++){
+        let [left,right] = survey[i].split("");
+        let result = Math.abs(choices[i]-4);
+        if(choices[i]>=4){
+            types[right] += result;
+        }
+        else if(choices[i]<4){
+            types[left] += result;
+        }
     }
-  }
-  const type = Object.keys(types);
-  for (let i = 0; i < type.length; i += 2) {
-    const leftType = types[type[i]];
-    const rightType = types[type[i + 1]];
-    if (leftType >= rightType) {
-      // 같거나 클 때는 왼쪽 요소
-      answer += type[i];
-    } else {
-      // 작은 때는 오른쪽 요소
-      answer += type[i + 1];
+    let type = Object.keys(types);
+    for(let i=0;i<type.length;i+=2){
+        if(types[type[i]]>=types[type[i+1]]){
+            answer+=type[i];
+        }
+        else answer+=type[i+1];
     }
-  }
-  return answer;
+    return answer;
 }
