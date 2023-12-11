f57b6aa372c1283ff3a3a63f99d64c3bc4af824d
Minju Kang <97059373+Minju9187@users.noreply.github.com>
2023-12-11 15:45:43 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.93 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -2,23 +2,28 @@ function solution(today, terms, privacies) {
     var answer = [];
     let object = {};
     let [today_year, today_month, today_day] = today.split(".");
+    //today 날짜 숫자로 변환
     today_year = Number(today_year);
     today_month = Number(today_month);
     today_day = Number(today_day);
+    //terms type,month 객체로 만들기
     for(let i=0;i<terms.length;i++){
         let [terms_type,terms_month] = terms[i].split(" ");
         object[terms_type] = terms_month;
     }
+    //privacies 돌면서 파기해야 할 정보 찾기
     for(let i=0;i<privacies.length;i++){
         let [date,type]=privacies[i].split(" ");
         let [year,month,day]= date.split(".");
         month = Number(month) + Number(object[type]);
         year = Number(year);
         day = Number(day);
+        //month가 12 넘을 때 year로 바꿔주기
         while(month>12){
             month = month-12;
             year = year + 1;
         }
+        
         if(today_year>year){
             answer.push(i+1);
         }
