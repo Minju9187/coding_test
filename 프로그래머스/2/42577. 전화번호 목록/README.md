a7e1d5486a5e10a51d20974dfd4e7888681de07b
Minju Kang <97059373+Minju9187@users.noreply.github.com>
2024-02-01 15:05:16 +0900

    [level 2] Title: 전화번호 목록, Time: 101.45 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/README.md
+++ 프로그래머스/2/42577. 전화번호 목록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전화번호 목록 - 42577 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577) 
 
 ### 성능 요약
 
-메모리: 75.7 MB, 시간: 97.06 ms
+메모리: 74.2 MB, 시간: 101.45 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 4일 9:51:59
+2024년 2월 4일 15:5:14
 
 ### 문제 설명
 
--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -3,6 +3,7 @@ function solution(phone_book) {
     let compare;
     
     phone_book.sort();
+    console.log(phone_book);
     for(let i=0;i<phone_book.length-1;i++){
         compare = phone_book[i];
         if(phone_book[i+1].indexOf(compare)===0){
@@ -11,3 +12,13 @@ function solution(phone_book) {
     }
     return answer;
 }
+
+// function solution(phone_book) {
+//     phone_book.sort()
+
+//     const isPrefix = phone_book.some((book,idx)=>{
+//         return phone_book[idx+1]?.indexOf(book) === 0
+//     })
+
+//     return !isPrefix
+// }   
