b2d99c333c098b2ad317d6b10f85f6a88c5dacc7
Minju Kang <97059373+Minju9187@users.noreply.github.com>
2024-02-19 10:45:09 +0900

    [level 2] Title: 땅따먹기, Time: 79.83 ms, Memory: 84.9 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.js
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.js
@@ -1,22 +1,9 @@
 function solution(land) {
-    var answer = 0;
-    let arr = [];
-    let value, idx, beforeIdx;
-    for(let i = 0 ; i < land.length ; i++){       
-        if(i===0){
-            value = Math.max(...land[i]);
-            idx = land[i].findIndex(v => v===value);
-        }else{
-            value = Math.max(...land[i]);
-            idx = land[i].findIndex(v => v===value);
-            if(beforeIdx === idx){
-                land[i].splice(idx,1);
-                value = Math.max(...land[i]);
-                idx = land[i].findIndex(v => v===value);
-            }
+    for (let i = 1; i < land.length; i++) {
+        for (let j = 0; j < 4; j++) {
+            //filter로 나랑 idx가 다른 위의 행에서 가장 큰 수를 찾아 더해줌
+            land[i][j] += Math.max(...land[i - 1].filter((_, idx) => j !== idx));
         }
-        answer += value;
-        beforeIdx = idx;
     }
-    return answer;
+    return Math.max(...land[land.length-1]);
 }
