7adc132f55bbfdf5a389a3f62bcf34f845cee745
강민주 <97059373+Minju9187@users.noreply.github.com>
2025-02-19 10:14:31 +0900

    [Gold II] Title: 후위 표기식, Time: 96 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Gold/1918. 후위 표기식/후위 표기식.js
@@ -0,0 +1,38 @@
+let fs = require("fs");
+let input = fs.readFileSync('/dev/stdin').toString().trim();
+// let input = fs.readFileSync("1.txt").toString().trim();
+
+const stack = [];
+let answer = "";
+
+input.split("").forEach((el) => {
+  if (el === "(") {
+    stack.push(el);
+  } else if (el === ")") {
+    while (stack.length && stack[stack.length - 1] !== "(") {
+      answer += stack.pop();
+    }
+    stack.pop();
+  } else if (el === "*" || el === "/") {
+    while (
+      stack.length &&
+      (stack[stack.length - 1] === "*" || stack[stack.length - 1] === "/")
+    ) {
+      answer += stack.pop();
+    }
+    stack.push(el);
+  } else if (el === "+" || el === "-") {
+    while (stack.length && stack[stack.length - 1] !== "(") {
+      answer += stack.pop();
+    }
+    stack.push(el);
+  } else {
+    answer += el;
+  }
+});
+
+while (stack.length) {
+  answer += stack.pop();
+}
+
+console.log(answer);
