c8fe552eb690a6ddf41bae79465029cbf18e1257
강민주 <97059373+Minju9187@users.noreply.github.com>
2025-02-24 10:24:27 +0900

    [Gold V] Title: 멍멍이 쓰다듬기, Time: 152 ms, Memory: 9852 KB -BaekjoonHub

---
+++ 백준/Gold/1669. 멍멍이 쓰다듬기/멍멍이 쓰다듬기.js
@@ -0,0 +1,29 @@
+let fs = require("fs");
+let input = fs.readFileSync('/dev/stdin').toString().trim();
+// let input = fs.readFileSync("1.txt").toString().trim();
+
+const [X, Y] = input.split(" ").map(Number);
+
+let dif = Y - X;
+let answer = 0;
+//1, 1 + 2 + 1(4), 1 + 2 + 3 + 2 + 1(9), 1 + 2 + 3 + 4 + 3 + 2 + 1(16)
+
+if (dif === 0) {
+  return console.log(0);
+}
+
+let idx = 0;
+while (idx * idx < dif) {
+  idx++;
+}
+
+idx = idx * idx === dif ? idx : idx - 1;
+answer += 2 * idx - 1;
+dif -= idx * idx;
+
+while (dif > 0) {
+  dif -= Math.min(idx, dif);
+  answer++;
+}
+
+console.log(answer);
